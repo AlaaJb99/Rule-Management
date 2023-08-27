97af6cc7299f84f45ff50a1b743eb34e1f88efb6
Alaa Jbareen <gbaren39@gmail.com>
2023-08-27 13:17:55 +0300

    template ready added a logDB file, server and routes

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.array());
+
+
+var log = require('./routes/log');
+app.use('/log', log);
+
+app.listen(8080);
