026d9bb7001741fb3b0e998f0fab943d612ef95e
GaliaY12 <142980299+GaliaY12@users.noreply.github.com>
2023-08-28 13:19:02 +0300

    schema_changed

--- backend/RulesDB.js
+++ backend/RulesDB.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/logAnalyzer');
 
 var logSchema = mongoose.Schema({
-    RuleName: String,
+    rule_name: String,
+    keywords :[]
 });
 
 var Log = mongoose.model("Rule", logSchema, 'log_analyzation');
