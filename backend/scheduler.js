5568431a58074ec901d2ff250698ca498d3f1953
Alaa Jbareen <gbaren39@gmail.com>
2023-08-28 21:07:46 +0300

    template the scheduler

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -3,14 +3,28 @@ const CronJob = require("node-cron");
 
 // Creating a cron job which runs on every 10 second
 exports.initScheduledJobs = () => {
-
     const scheduledJobFunction = CronJob.schedule("*/60 * * * * *", function () {
         console.log("running a task every 60 second");
         //Our logic check if there are new files collection in DB
         //untill make the check according to the name of the collections 
         //the file collection begins with file_<file_name>
         // Start Here =>
-        //
+        // [file_log1, file_log2]
+
+
+        //get all the collection 
+
+
+        
+        const collection_names=[];
+        //check the collection names
+
+
+
+
+
+        analyze(collection_names);
+
         
     });
 
