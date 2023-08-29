d4bb385ad2f4001173e871a951b01382743cce0f
GaliaY12 <142980299+GaliaY12@users.noreply.github.com>
2023-08-29 14:39:02 +0300

    rank giving raw according to his specified analyzed rule and ranks

--- backend/analyzation/rankAnalyze.js
+++ backend/analyzation/rankAnalyze.js
@@ -1,2 +1,51 @@
+var ranks = require('./ranks.js');
 
 
+function ranksAnalyzation(raw, rules)
+{
+
+    var raw_rule=raw.rule;
+    /// in defualt rank will be 1-low ,(just be sure about unkown rule if to put 0 for rank or 1!)
+    var res_rank=1;
+    let ruleObject = null;
+    for (const rule of rules) {
+        if (rule.rule === raw_rule) {
+            ruleObject = rule;
+            break;
+        }
+    }
+    if(ruleObject)
+    {
+        console.log("Entered ");
+        var keywords_array=ruleObject.keywords;
+        var hist=[0,0,0];
+        for (const keyword_ of keywords_array) {
+            var ranks1=ranks[0][1];
+            var ranks2=ranks[1][2];
+            var ranks3=ranks[2][3];
+            if (ranks1.includes(keyword_)) {
+                hist[0]++ ;
+            }
+            else if(ranks2.includes(keyword_))
+            {
+                hist[1]++ ;
+            }
+            else if(ranks3.includes(keyword_))
+            {
+                hist[2]++ ;
+            }
+        }
+        var max_rank = 1;
+        for (let i = 1; i < hist.length; i++) {
+            if (hist[i] > hist[max_rank-1]) {
+                max_rank = i+1;
+            }
+        }
+        res_rank=max_rank;
+
+    }
+    raw.rank=res_rank;
+    return raw;
+}
+
+module.exports = ranksAnalyzation;
--- backend/analyzation/ranks.js
+++ backend/analyzation/ranks.js
@@ -1,23 +1,25 @@
-export const ranks =
-    [{
-        1: [
-            "Success", "Transaction",
-            "Resource", "Memory", "Disk",
-            "Success", "Transaction",
-            "Application", "Service", "Component",
-            "Info"
-        ]
-    },//low
-    {
-        2: [
-            "Warning", "Network", "Connection", "Latency", "Bottleneck", "Load", "Latency", "Slow", "Delay", "Network Down", "Connection Lost"]
-    },//medium
-    {
-        3: [
-            "Unauthorized", "Access Denied", "Intrusion", "Unusual", "Abnormal", "Anomaly", "Malware", "Exploit", "Attack",
-            "High Usage", "Exceeded Limit",
-            "Outage", "Downtime", "Unavailable",
-            "Critical", "Error", "Exception", "Failure", "Fatal",
-            "Privacy Breach", "Data Leak", "Authentication", "Authorization", "Vulnerability", "Hardware Failure", "Component Error"
-        ]
-    }];//high
+const ranks =
+[{
+    1: [
+        "Success", "Transaction",
+        "Resource", "Memory", "Disk",
+        "Success", "Transaction",
+        "Application", "Service", "Component",
+        "Info"
+    ]
+},//low
+{
+    2: [
+        "Warning", "Network", "Connection", "Latency", "Bottleneck", "Load", "Latency", "Slow", "Delay", "Network Down", "Connection Lost"]
+},//medium
+{
+    3: [
+        "Unauthorized", "Access Denied", "Intrusion", "Unusual", "Abnormal", "Anomaly", "Malware", "Exploit", "Attack",
+        "High Usage", "Exceeded Limit",
+        "Outage", "Downtime", "Unavailable",
+        "Critical", "Error", "Exception", "Failure", "Fatal",
+        "Privacy Breach", "Data Leak", "Authentication", "Authorization", "Vulnerability", "Hardware Failure", "Component Error"
+    ]
+}];//high
+
+module.exports =ranks;
