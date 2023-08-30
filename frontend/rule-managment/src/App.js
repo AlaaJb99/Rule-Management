fc50a95a7515d73f82071598766901bd54bfc290
Alaa Jbareen <gbaren39@gmail.com>
2023-08-30 13:50:05 +0300

    added the ruleTable file and the css design

--- frontend/rule-managment/src/App.js
+++ frontend/rule-managment/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import RuleTable from './RuleTable';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const rules = [
+  { rule: 'Error', level: 'low', date: '15-8-2023', Message: 'jjj' },
+  { rule: 'Exception', level: 'Medium', date: '16-8-2023', Message: 'kkk' },
+  { rule: 'Exception', level: 'High', date: '15-9-2023', Message: 'lll' },
+  { rule: 'Error', level: 'low', date: '10-10-2023', Message: 'mmm' }
+];
 
+function App() {
+  const [sortedBy, setSortedBy] = useState(null);
+
+  const handleSortChange = (event) => {
+    setSortedBy(event.target.value);
+  };
+
+  const sortedRules = rules.slice().sort((a, b) => {
+    if (sortedBy === 'level') {
+      const levelOrder = { low: 1, medium: 2, high: 3 };
+      return levelOrder[a.level.toLowerCase()] - levelOrder[b.level.toLowerCase()];
+    }else if (sortedBy === 'date') {
+      return new Date(a.date) - new Date(b.date);
+    }
+    return 0;
+  });
+
+  return (
+    <div className='mainP'>
+    <h1>Your Table</h1>
+    <label htmlFor="sortOptions">Sort By: </label>
+    <select id="sortOptions" onChange={handleSortChange}>
+      <option value="">None</option>
+      <option value="level">Level</option>
+      <option value="date">Date</option>
+    </select>
+    <div className="App">
+      <RuleTable rules={sortedRules} />
+    </div>
+  </div>
+);
+}
 export default App;
