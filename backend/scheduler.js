5c197184f73c564c692053a65fe8c233fa638dab
Alaa Jbareen <gbaren39@gmail.com>
2023-08-30 13:22:15 +0300

    Added the condition

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -1,5 +1,12 @@
 var express = require('express');
 const CronJob = require("node-cron");
+const mongoose = require("mongoose");
+
+//connect to database 
+mongoose.connect("mongodb://localhost:27017/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Creating a cron job which runs on every 10 second
 exports.initScheduledJobs = () => {
@@ -11,7 +18,23 @@ exports.initScheduledJobs = () => {
         //the file collection begins with file_<file_name>
         // Start Here =>
         //
-        
+
+        const fileCollections = [];
+        mongoose.connection.on('open', function (ref) {
+            console.log('Connected to mongo server.');
+            //trying to get collection names
+            mongoose.connection.db.listCollections().toArray(function (err, names) {
+                console.log(names); // [{ name: 'dbname.myCollection' }]
+                const pattern = /^file_/;
+                for (const name of names) {
+
+                    if (name.match(pattern)) {
+                        fileCollections.push(name);
+                    }
+                }
+            });
+        });
+        console.log(fileCollections);
     });
 
     scheduledJobFunction.start();
