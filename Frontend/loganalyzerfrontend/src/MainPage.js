d8f6030cf2e08945757be17a48009df8371e6b2b
Alaa Jbareen <gbaren39@gmail.com>
2023-08-31 09:06:21 +0300

    added usestate for two arrays one for the rules and one for the files these are responsiable for holding the data from the backend

--- Frontend/loganalyzerfrontend/src/MainPage.js
+++ Frontend/loganalyzerfrontend/src/MainPage.js
@@ -1,42 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import './Loading.css';
 
 function MultiSelect() {
-  const ruleOptions = [
-    { value: 'Error Detection', label: 'Error Detection' },
-    { value: 'Warning Identification', label: 'Warning Identification' },
-    { value: 'Authentication Issue', label: 'Authentication Issue' },
-    { value: 'Network Anomalies', label: 'Network Anomalies' },
-    { value: 'Performance Bottlenecks', label: 'Performance Bottlenecks' },
-    { value: 'Security Breach Attempt', label: 'Security Breach Attempt' },
-    { value: 'Resource Monitoring', label: 'Resource Monitoring' },
-    { value: 'Successful Transactions', label: 'Successful Transactions' },
-    { value: 'Application Events', label: 'Application Events' },
-    { value: 'Informational Logs', label: 'Informational Logs' },
-    { value: 'Unauthorized Access', label: 'Unauthorized Access' },
-    { value: 'Anomaly Detection', label: 'Anomaly Detection' },
-    { value: 'Malicious Activity', label: 'Malicious Activity' },
-    { value: 'Resource Exceedance', label: 'Resource Exceedance' },
-    { value: 'Latency Threshold', label: 'Latency Threshold' },
-    { value: 'Outages and Downtime', label: 'Outages and Downtime' },
-    { value: 'Critical Errors', label: 'Critical Errors' },
-    { value: 'Successful Transactions', label: 'Successful Transactions' },
-    { value: 'Regulatory Compliance', label: 'Regulatory Compliance' },
-    { value: 'Data Privacy', label: 'Data Privacy' },
-    { value: 'Network Connectivity', label: 'Network Connectivity' },
-    { value: 'Hardware Failure', label: 'Hardware Failure' },
-  ];
-  const documentOptions = [
-    { value: 'a', label: 'Document A' },
-    { value: 'b', label: 'Document B' },
-    { value: 'c', label: 'Document C' },
-    
-  ];
+  const [ruleOptions, setRuleOptions] = useState([]);
+  // [
+  //   { value: 'Error Detection', label: 'Error Detection' },
+  //   { value: 'Warning Identification', label: 'Warning Identification' },
+  //   { value: 'Authentication Issue', label: 'Authentication Issue' },
+  //   { value: 'Network Anomalies', label: 'Network Anomalies' },
+  //   { value: 'Performance Bottlenecks', label: 'Performance Bottlenecks' },
+  //   { value: 'Security Breach Attempt', label: 'Security Breach Attempt' },
+  //   { value: 'Resource Monitoring', label: 'Resource Monitoring' },
+  //   { value: 'Successful Transactions', label: 'Successful Transactions' },
+  //   { value: 'Application Events', label: 'Application Events' },
+  //   { value: 'Informational Logs', label: 'Informational Logs' },
+  //   { value: 'Unauthorized Access', label: 'Unauthorized Access' },
+  //   { value: 'Anomaly Detection', label: 'Anomaly Detection' },
+  //   { value: 'Malicious Activity', label: 'Malicious Activity' },
+  //   { value: 'Resource Exceedance', label: 'Resource Exceedance' },
+  //   { value: 'Latency Threshold', label: 'Latency Threshold' },
+  //   { value: 'Outages and Downtime', label: 'Outages and Downtime' },
+  //   { value: 'Critical Errors', label: 'Critical Errors' },
+  //   { value: 'Successful Transactions', label: 'Successful Transactions' },
+  //   { value: 'Regulatory Compliance', label: 'Regulatory Compliance' },
+  //   { value: 'Data Privacy', label: 'Data Privacy' },
+  //   { value: 'Network Connectivity', label: 'Network Connectivity' },
+  //   { value: 'Hardware Failure', label: 'Hardware Failure' },
+  // ];
+  const [documentOptions, setDocumentOptions] = useState([]);
+
+  // [
+  //   { value: 'a', label: 'Document A' },
+  //   { value: 'b', label: 'Document B' },
+  //   { value: 'c', label: 'Document C' },
+
+  // ];
 
   const [selectedRules, setSelectedRules] = useState([]);
-  const [selectedDocument, setSelectedDocument] = useState(null); 
+  const [selectedDocument, setSelectedDocument] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+
+
+  //useEffect to get the log files and rules from the backend
+  useEffect(() => {
+    //GET http request to get all the avaiable rules and files
+    fetch('http://localhost:8080/rule')
+      .then(response => response.json())
+      .then(data => setRuleOptions(data));
+    fetch('http://localhost:8080/file')
+      .then(response => response.json())
+      .then(data => setDocumentOptions(data));
+
+  }, []);
 
   const handleRuleSelectChange = (selectedRules) => {
     setSelectedRules(selectedRules);
--- Frontend/loganalyzerfrontend/src/index.js
+++ Frontend/loganalyzerfrontend/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MultiSelect from './MainPage';
 //import SecondPage from './SecondPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MultiSelect  />
+    <App  />
   </React.StrictMode>
 );
 
